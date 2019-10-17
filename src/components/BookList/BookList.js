8046f166684f6c6676d1ae827f8729b99a3276c7
rgee258 <9219093+rgee258@users.noreply.github.com>
2019-10-16 23:02:09 -0400

    Improved state for book management, setup basic BookList rendering, removed utils

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,1 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './BookList.css'
+
+class BookList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="book-list">
+        {this.props.bookIds.map((id, index) =>
+          <div key={id} className="book">
+            <div className="title">{this.props.titles[id]}</div>
+            <div className="author">{this.props.authors[id]}</div>
+            <div className="pages">{this.props.pages[id]}</div>
+            <div className="readStatus">{this.props.readStatus[id]}</div>
+            {/* TODO Button for updating readStatus */}
+            {/* TODO Button for deleting book */}
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+BookList.propTypes = {
+  // TODO Add proptypes
+}
+
+export default BookList
